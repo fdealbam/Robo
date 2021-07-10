a9a489f4c4e91dfb926794b41d95309873454d89
Felipe de Alba <45267327+fdealbam@users.noreply.github.com>
2021-07-10 18:58:19 -0500

    Update dash.py

--- application/dash.py
+++ application/dash.py
@@ -30,7 +30,11 @@ yea = datetime.strftime(yesterday, '%Y%m%d')
 today = date.today()
 d2 = today.strftime("Fecha de actualización : %d-%m-%Y")
 
-
+tabla1 = pd.read_csv('https://raw.githubusercontent.com/fdealbam/violenciadegenero/main/Tabla1.csv')              
+tabla1_f = tabla1[tabla1['Tipo de delito']== 'Violación' ]
+tabla1_f.reset_index(inplace=True,)
+TOTABUSOSEXUAL = tabla1_f.iloc[0]['GRAND TOTAL']
+TASAABUSOSEXUAL = tabla1_f.iloc[0]['tasa_acumulada']
 
 ###############################
 # DATABASES
@@ -398,6 +402,12 @@ body = html.Div([
                     "El abuso sexual es uno de los delitos más graves de la violencia de género que se vive en el país, "
                     "además, son problemas aún irresueltos y son tema central de la " 
                     "agenda legislativa, pero hoy alcanzan relevancia en la agenda seguridad pública del país, también. "
+                   " Entre 2015 y 2021 se registraron "+ str(f"{int(TOTABUSOSEXUAL):,}") +" casos, lo que representa una tasa de "+
+       str(TASAABUSOSEXUAL) +" delitos por cada 100 mil habitantes. "+
+      "En este dashboard analítico observamos su gravedad según intervalos anuales e intervalos mensuales"+
+      "; también incluimos un análisis detallado de las cuatro entidades con más incidencias en este delito"+
+      "; finalmente, comparamos los rankings por entidad según sumas acumuladas respecto a las tasas, ambas "+
+      "por entidad y ambas del periódo 2015 al 2021.",
                     "Este dashboard analítico se compone de una sección en la cual tratamos el abuso sexual, observamos "
                     "su gravedad según intervalos anuales o mensuales; incluimos el análisis detallado de cuatro "
                     "entidades con más incidencias de este delito; finalmente, comparamos los rankings por entidad "
