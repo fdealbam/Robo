cb5d306e282feaeb9e626d83689390bd7b2833c8
Felipe de Alba <45267327+fdealbam@users.noreply.github.com>
2021-09-14 11:09:05 -0500

    Update dash.py

--- application/dash.py
+++ application/dash.py
@@ -404,7 +404,7 @@ body = html.Div([
                     "agenda legislativa, pero hoy alcanzan relevancia en la agenda seguridad pública del país, también. "
                    " Entre 2015 y 2021 se registraron "+ str(f"{int(TOTABUSOSEXUAL):,}") +" casos, lo que representa una tasa de "+
        str(TASAABUSOSEXUAL) +" delitos por cada 100 mil habitantes. "+
-      "En este dashboard analítico observamos su gravedad según intervalos anuales e intervalos mensuales"+
+      "En este tablero analítico observamos su gravedad según intervalos anuales e intervalos mensuales"+
       "; también incluimos un análisis detallado de las cuatro entidades con más incidencias en este delito"+
       "; finalmente, comparamos los rankings por entidad según sumas acumuladas respecto a las tasas, ambas "+
       "por entidad y ambas del periódo 2015 al 2021.",
@@ -712,14 +712,14 @@ body = html.Div([
                 html.Br(),
                 html.H4("Metodología "),
                 html.P(
-                    "El presente dashboard es un ejercicio institucional con el objeto de "
+                    "El presente tablero es un ejercicio institucional con el objeto de "
                     "informar a las diputadas y diputados y público interesado sobre un tema "
                     "de vital importancia en la vida política. "
                     "La metodología que hemos empleado para analizar los datos la detallamos enseguida. "
                     "Como se indica en cada caso, la información sobre el delito abuso sexual proviene del Secretariado "
                     "Ejecutivo Nacional del Sistema Nacional de Seguridad Pública (SENSNSP) (2015-2021); "
                     " "
-                    "Este dashboard seguramente será completado progresivamente con otras fuentes de información "
+                    "Este tablero seguramente será completado progresivamente con otras fuentes de información "
                     "tanto gubernamental, como aquella proveniente de organizaciones civiles que " 
                     "dan seguimiento al tema. "
                     "En ningún caso, este contenido representa algún "
